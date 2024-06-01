0c2762ccb74d6674165514ac8ae546c4f6cceef5
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-01 05:19:21 +0300

    Update useAtomicBindingManifest.ts

--- src/useAtomicBindingManifest.ts
+++ src/useAtomicBindingManifest.ts
@@ -1,5 +1,4 @@
-import type { Manifest, ManifestInstances } from "@rbxts/atomic-binding";
-import { AtomicBinding } from "@rbxts/atomic-binding";
+import { AnyManifestRoot, AtomicBinding, Manifest, ManifestInstances } from "@rbxts/atomic-binding";
 import { useHookState } from "@rbxts/matter";
 
 type Storage = {
@@ -16,29 +15,30 @@ function cleanup(storage: Storage) {
 	storage.value.binding.unbindRoot(storage.value.root);
 }
 
-export function useAtomicBindingManifest<R extends Instance = Instance, M extends Manifest<R> = Manifest<R>>(
-	root: R,
-	manifest: M,
-    discriminator?: unknown,
-) {
+export function useAtomicBindingManifest<
+	R extends AnyManifestRoot = AnyManifestRoot,
+	M extends Manifest<any> = Manifest<R>,
+>(root: R, manifest: M, discriminator?: unknown) {
 	const storage = useHookState(discriminator, cleanup) as Storage;
 
 	if (!storage.value) {
+		const binding = new AtomicBinding<R, M>(manifest, (instances) => {
+			value.instances = instances;
+
+			return () => {
+				if (!storage.value) return;
+
+				storage.value.instances = undefined;
+			};
+		});
+
+		binding.bindRoot(root);
+
 		const value: NonNullable<Storage["value"]> = {
 			instances: undefined,
-			binding: new AtomicBinding<R, M>(manifest, (instances) => {
-				value.instances = instances as any;
-
-				return () => {
-					if (!storage.value) return;
-
-					storage.value.instances = undefined;
-				};
-			}),
+			binding,
 			root,
 		};
-
-		value.binding.bindRoot(root);
 
 		storage.value = value;
 	}
