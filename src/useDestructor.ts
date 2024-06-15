c0bf61089fef235f87407eda92ac9df7b2578cf6
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-15 16:49:54 +0300

    add dependencies version of "useDestructor" hook

--- src/useDestructor.ts
+++ src/useDestructor.ts
@@ -1,17 +1,47 @@
 import { useHookState } from "@rbxts/matter";
+import { equals, isArray } from "@rbxts/sift/out/Array";
+
+type Dtor = Callback;
+type DtorCallback = () => Dtor;
 
 type Storage = {
-	dtor?: Callback;
+	dtor?: Dtor;
+};
+
+type StorageWithDependencies = Storage & {
+	dependencies?: object;
 };
 
 function cleanup(storage: Storage) {
 	if (storage.dtor) storage.dtor();
 }
 
-export function useDestructor(callback: () => Callback, discriminator?: unknown) {
-	const storage = useHookState(discriminator, cleanup) as Storage;
+/**
+ * Do not pass a discriminator of the `table` type. This will cause the underlying code flow to interpret this as `dependencies` without the discriminator being passed as 3rd argument.
+ */
+export function useDestructor(callback: DtorCallback, discriminator?: unknown): void;
 
-	if (!storage.dtor) {
-		storage.dtor = callback();
+/**
+ * Whenever the `dependencies` change the destructor is called.
+ */
+export function useDestructor(callback: DtorCallback, dependencies: Array<unknown>, discriminator?: unknown): void;
+
+export function useDestructor(callback: DtorCallback, dependenciesOrDiscriminator?: unknown, discriminator?: unknown) {
+	if (typeIs(dependenciesOrDiscriminator, "table")) {
+		const storage = useHookState(discriminator, cleanup) as StorageWithDependencies;
+
+		const dependencies = dependenciesOrDiscriminator;
+
+		if (!storage.dtor || !equals(dependencies, storage.dependencies)) {
+			storage.dtor = callback();
+
+			storage.dependencies = dependencies;
+		}
+	} else {
+		const storage = useHookState(dependenciesOrDiscriminator, cleanup) as Storage;
+
+		if (!storage.dtor) {
+			storage.dtor = callback();
+		}
 	}
 }
