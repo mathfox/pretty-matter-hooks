41dc7a514a18989bc934291c7db8e91a8395ae9d
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-04 02:31:34 +0300

    sort exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./useAsync";
 export * from "./useAtom";
-export * from "./useState";
 export * from "./useAtomicBindingManifest";
 export * from "./useChange";
 export * from "./useContextAction";
@@ -9,3 +8,4 @@ export * from "./useLastInputType";
 export * from "./useLocalPlayerGui";
 export * from "./useMap";
 export * from "./useMemo";
+export * from "./useState";
