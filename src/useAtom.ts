c7dea1726ffddd3dcfc4f67d22afaf598f84339b
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-18 02:48:55 +0300

    update deps

--- src/useAtom.ts
+++ src/useAtom.ts
@@ -2,7 +2,7 @@ import { type Molecule, subscribe } from "@rbxts/amulet";
 import { useDestructor } from "./useDestructor";
 import { useState } from "./useState";
 
-export function useAtom<T>(molecule: Molecule<T>, discriminator?: unknown) {
+export function useAtom<T>(molecule: Molecule<T>, discriminator?: unknown): T {
 	const [state, setState] = useState(molecule, discriminator);
 
 	useDestructor(
